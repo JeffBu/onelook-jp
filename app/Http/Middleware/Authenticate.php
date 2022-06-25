40587a08e61c2cebfcbda2ad12f674a46ddec314
jvn-jeferson <jbonayon15@gmail.com>
2022-06-25 09:56:06 +0800

    fixed redirection to home page

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -18,7 +18,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('landing');
+            return redirect()->route('landing');
         }
     }
 }
