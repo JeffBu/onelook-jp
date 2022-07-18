2b0b92025c570bcc73be470a137e3cc709c59ca4
jvn-jeferson <jbonayon15@gmail.com>
2022-07-18 14:28:54 +0800

    added a default access key generation function to saving

--- app/Http/Controllers/VideoRecordingEvents.php
+++ app/Http/Controllers/VideoRecordingEvents.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
 use App\Models\VideoRecord;
 use App\Models\VideoView;
+use App\Models\VideoAccess;
 use Illuminate\Support\Facades\Validator;
 use Google\Cloud\Storage\StorageClient;
 
