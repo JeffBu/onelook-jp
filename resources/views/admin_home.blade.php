0d8aa58671c3b5f80868ac18b44997a475d93343
jdecena08 <jaezoff.d@jvn-cgs.com>
2022-07-26 14:59:41 +0800

    update: header layout | update: dashboard layout | update: admin_dashboard layout

--- resources/views/admin_home.blade.php
+++ resources/views/admin_home.blade.php
@@ -29,11 +29,10 @@
 </head>
 
 <body class="justify-center shadow-sm items-center bg-theme-white text-theme-black font-['Calibri']">
-
     <!--content-->
-    <div class="flex flex-row justify-center items-center min-h-screen w-full">
-        <nav id="menu" class="hidden sm:flex flex-col px-2 justify-left items-center text-center w-72 nav-bg shadow-md divide-y divide-theme-white min-h-screen">
-            <div class="flex justify-left px-4 py-6 items-center text-xl font-semibold text-theme-white gap-3 w-full">
+    <div class="flex flex-row justify-center items-start w-full">
+        <nav id="menu" class="hidden sm:flex flex-col px-2 justify-left items-center text-center w-72 nav-bg shadow-md divide-y divide-theme-white min-h-screen fixed md:sticky top-0 left-0">
+            <div class="flex justify-left px-4 py-6 pt-16 md:pt-6 items-center text-xl font-semibold text-theme-white gap-3 w-full">
                 {{config('app.name')}}
             </div>
 
@@ -70,18 +69,10 @@
             </div>
         </nav>
 
-        <div id="content" class="flex flex-col justify-center items-center w-full">
-            <div class="flex justify-between items-center px-4 header-bg h-12 w-full sticky top-0 z-30">
+        <div class="flex flex-col justify-center items-center w-full">
+            <div class="flex justify-between items-center px-4 header-bg h-11 w-full sticky top-0 z-30">
                 <div class="flex justify-start items-center gap-8">
-                    <div class="hidden sm:flex justify-start items-center">
-                        <button id="close-button" onclick="closeMenu()" class="flex"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-                        </svg></button>
-                        <button id="open-button" onclick="openMenu()" class="hidden"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-                        </svg></button>
-                    </div>
-                    <button id="menu-button" onclick="toggleMenu()" class="flex sm:hidden"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <button id="menu-button" onclick="toggleMenu()"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                     </svg></button>
                     <h1 class="text-lg font-semibold text-theme-white">ホーム</h1>
@@ -89,121 +80,119 @@
                 <h1 class="text-lg font-semibold text-theme-white">管理画面</h1>
             </div>
 
-            <div class="flex flex-col justify-center items-center mt-8 w-3/4 z-10">
-                <div class="text-left w-full">
-                    <h1 class="text-xl font-semibold text-lime-600">登録状況</h1>
+            <div onclick="hideMenu()" class="flex justify-center items-center w-full">
+                <div class="flex flex-col justify-center items-center mt-8 w-3/4 z-10">
+                    <div class="text-left w-full">
+                        <h1 class="text-xl font-semibold text-lime-600">登録状況</h1>
+                    </div>
+
+                    <table class="text-center mt-6 border border-lime-600 w-full">
+                        <thead>
+                            <tr>
+                                <th rowspan="2" class="px-4 py-1 border border-lime-700">10月</th>
+                                <th rowspan="2" class="px-4 py-1 border border-lime-700">会員数合計</th>
+                                <th colspan="4" class="px-4 py-1 border border-lime-700">動画数</th>
+                            </tr>
+                            <tr>
+                                <th class="px-4 py-1 border border-lime-700">作成動画</th>
+                                <th class="px-4 py-1 border border-lime-700">期限内動画</th>
+                                <th class="px-4 py-1 border border-lime-700">招待メール</th>
+                                <th class="px-4 py-1 border border-lime-700">閲覧数</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td class="px-4 py-1 border border-lime-700">フリープラン</td>
+                                <td class="px-4 py-1 border border-lime-700">30</td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                            </tr>
+                            <tr>
+                                <td class="px-4 py-1 border border-lime-700">パーソナルプラン</td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                            </tr>
+                            <tr>
+                                <td class="px-4 py-1 border border-lime-700">ビジネスプラン</td>
+                                <td class="px-4 py-1 border border-lime-700">300</td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                            </tr>
+                            <tr class="header-bg text-theme-white">
+                                <td class="px-4 py-1 border border-lime-700">合計</td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <table class="text-center mt-6 border border-lime-600 w-full">
+                        <thead>
+                            <tr>
+                                <th rowspan="2" class="px-4 py-1 border border-lime-700">9月</th>
+                                <th rowspan="2" class="px-4 py-1 border border-lime-700">会員数合計</th>
+                                <th colspan="4" class="px-4 py-1 border border-lime-700">動画数</th>
+                            </tr>
+                            <tr>
+                                <th class="px-4 py-1 border border-lime-700">作成動画</th>
+                                <th class="px-4 py-1 border border-lime-700">期限内動画</th>
+                                <th class="px-4 py-1 border border-lime-700">招待メール</th>
+                                <th class="px-4 py-1 border border-lime-700">閲覧数</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td class="px-4 py-1 border border-lime-700">フリープラン</td>
+                                <td class="px-4 py-1 border border-lime-700">30</td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                            </tr>
+                            <tr>
+                                <td class="px-4 py-1 border border-lime-700">パーソナルプラン</td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                            </tr>
+                            <tr>
+                                <td class="px-4 py-1 border border-lime-700">ビジネスプラン</td>
+                                <td class="px-4 py-1 border border-lime-700">300</td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                            </tr>
+                            <tr class="header-bg text-theme-white">
+                                <td class="px-4 py-1 border border-lime-700">合計</td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                                <td class="px-4 py-1 border border-lime-700"></td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <div class="pt-32"></div>
                 </div>
-
-                <table class="text-center mt-6 border border-lime-600 w-full">
-                    <thead>
-                        <tr>
-                            <th rowspan="2" class="px-4 py-1 border border-lime-700">10月</th>
-                            <th rowspan="2" class="px-4 py-1 border border-lime-700">会員数合計</th>
-                            <th colspan="4" class="px-4 py-1 border border-lime-700">動画数</th>
-                        </tr>
-                        <tr>
-                            <th class="px-4 py-1 border border-lime-700">作成動画</th>
-                            <th class="px-4 py-1 border border-lime-700">期限内動画</th>
-                            <th class="px-4 py-1 border border-lime-700">招待メール</th>
-                            <th class="px-4 py-1 border border-lime-700">閲覧数</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        <tr>
-                            <td class="px-4 py-1 border border-lime-700">フリープラン</td>
-                            <td class="px-4 py-1 border border-lime-700">30</td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                        </tr>
-                        <tr>
-                            <td class="px-4 py-1 border border-lime-700">パーソナルプラン</td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                        </tr>
-                        <tr>
-                            <td class="px-4 py-1 border border-lime-700">ビジネスプラン</td>
-                            <td class="px-4 py-1 border border-lime-700">300</td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                        </tr>
-                        <tr class="header-bg text-theme-white">
-                            <td class="px-4 py-1 border border-lime-700">合計</td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                        </tr>
-                    </tbody>
-                </table>
-
-                <table class="text-center mt-6 border border-lime-600 w-full">
-                    <thead>
-                        <tr>
-                            <th rowspan="2" class="px-4 py-1 border border-lime-700">9月</th>
-                            <th rowspan="2" class="px-4 py-1 border border-lime-700">会員数合計</th>
-                            <th colspan="4" class="px-4 py-1 border border-lime-700">動画数</th>
-                        </tr>
-                        <tr>
-                            <th class="px-4 py-1 border border-lime-700">作成動画</th>
-                            <th class="px-4 py-1 border border-lime-700">期限内動画</th>
-                            <th class="px-4 py-1 border border-lime-700">招待メール</th>
-                            <th class="px-4 py-1 border border-lime-700">閲覧数</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        <tr>
-                            <td class="px-4 py-1 border border-lime-700">フリープラン</td>
-                            <td class="px-4 py-1 border border-lime-700">30</td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                        </tr>
-                        <tr>
-                            <td class="px-4 py-1 border border-lime-700">パーソナルプラン</td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                        </tr>
-                        <tr>
-                            <td class="px-4 py-1 border border-lime-700">ビジネスプラン</td>
-                            <td class="px-4 py-1 border border-lime-700">300</td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                        </tr>
-                        <tr class="header-bg text-theme-white">
-                            <td class="px-4 py-1 border border-lime-700">合計</td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                            <td class="px-4 py-1 border border-lime-700"></td>
-                        </tr>
-                    </tbody>
-                </table>
-
             </div>
-
         </div>
-
     </div>
-
-    <div class="pt-20"></div>
-
     <!--content ends here-->
 
     <!--script-->
@@ -243,23 +232,16 @@
             $('#home').addClass('active');
         });
 
-        function closeMenu() {
-            $('#menu').toggle();
-            $('#close-button').toggle();
-            $('#open-button').toggle();
-            $('#content').css('margin-left', '0');
-        }
-
-        function openMenu() {
-            $('#menu').toggle();
-            $('#close-button').toggle();
-            $('#open-button').toggle();
-            $('#content').css('margin-left', '256px');
-        }
-
         function toggleMenu() {
             $('#menu').toggle();
             $('#menu').css('z-index', '20');
+        }
+
+        function hideMenu() {
+            if ($(window).width() < 768) {
+                $('#menu').hide();
+                $('#menu').css('z-index', '20');
+            }
         }
 
     </script>
