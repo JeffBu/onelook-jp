3231ed81d6f549893986997d8d1de232d22aa405
jdecena08 <jaezoff.d@jvn-cgs.com>
2022-07-26 10:59:04 +0800

    update: change_password layout

--- resources/views/edit_personal_info.blade.php
+++ resources/views/edit_personal_info.blade.php
@@ -41,44 +41,69 @@
     <!--header ends here-->
 
     <!--content-->
-    <div>
-        <h1 class="text-center text-3xl font-bold text-cyan-600 pb-10 pt-20">ログインパスワードを変更します。</h1>
-    </div>
+    <div class="flex flex-col justify-center items-center pt-20 gap-8">
+        <div class="flex justify-center items-center w-1/3 h-modal md:h-full">
+            <div class="relative p-4 w-full max-w-3xl h-full md:h-auto">
+                <!--modal content-->
+                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    <!--modal header-->
+                    <div class="flex justify-between items-center p-5 rounded-t border-b dark:border-gray-600">
+                        <h3 class="text-xl font-bold text-sky-600 dark:text-white">
+                            ログインパスワードを変更します。
+                        </h3>
 
-    <div class="flex justify-center items-center">
-        <table class="text-center w-1/2 border border-sky-700">
-            <tbody>
-                <tr>
-                    <th class="px-1 py-1 border-x border-y border-cyan-600 w-2/5">ログインID（変更できません）</td>
-                    <td class="px-1 py-1 border-x border-y border-cyan-600">dad123</td>
-                </tr>
+                    </div>
+                    <!--modal body-->
+                    <div class="px-8 py-4 space-y-4">
+                        <form action="{{route('register')}}" method="POST">
+                            @csrf
+                            <div class="relative z-0 w-full mb-4 group">
+                                <input type="text" name="company_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                                <label for="company_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">新パスワード</label>
+                            </div>
+                            <div class="relative z-0 w-full mb-4 group">
+                                <input type="text" name="full_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                                <label for="full_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">新パスワード（確認用）</label>
+                            </div>
+                            <!--<div class="relative z-0 w-full mb-4 group">
+                                <input type="text" name="address" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                                <label for="address" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">会社名（任意）</label>
+                            </div>
+                            <div class="relative z-0 w-full mb-4 group">
+                                <input type="text" name="phone_number" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                                <label for="phone_number" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">電話番号</label>
+                            </div>
+                            <div class="grid xl:grid-cols-2 xl:gap-6">
+                                <div class="relative z-0 w-full mb-4 group">
+                                    <input type="text" name="username" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                                    <label for="username" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">ユーザー名</label>
+                                </div>
+                                <div class="relative z-0 w-full mb-4 group">
+                                    <input type="email" name="email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                                    <label for="email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">メールアドレス</label>
+                                </div>
+                            </div>
+                            <div class="grid xl:grid-cols-2 xl:gap-6">
+                                <div class="relative z-0 w-full mb-4 group">
+                                    <input type="password" name="password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                                    <label for="password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">パスワード</label>
+                                </div>
+                                <div class="relative z-0 w-full mb-4 group">
+                                    <input type="password" name="password_confirmation" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                                    <label for="password_confirmation" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">パスワードを認証する</label>
+                                </div>
+                            </div>-->
+                            <div class="relative z-0 w-full mb-4 group">
+                                <x-jet-validation-errors class="mb-4" />
+                            </div>
 
-                <tr>
-                    <th class="px-1 py-1 border-x border-y border-cyan-600 w-2/5">旧パスワード</td>
-                    <td class="px-1 py-1 border-x border-y border-cyan-600">********（変更日：2021/09/30）</td>
-                </tr>
-
-                <tr>
-                    <th class="px-1 py-1 border-x border-y border-cyan-600 w-2/5">新パスワード</td>
-                    <td class="px-1 py-1 border-x border-y border-cyan-600">
-                        <input type="password" class="w-full text-center px-1 py-1">
-                    </td>
-                </tr>
-
-                <tr>
-                    <th class="px-1 py-1 border-x border-y border-cyan-600 w-2/5">新パスワード（確認用）</td>
-                    <td class="px-1 py-1 border-x border-y border-cyan-600">
-                        <input type="password" class="w-full text-center px-1 py-1">
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-
-    <div class="flex justify-center items-center w-screen">
-        <div class="justify-end items-center w-1/2">
-            <div class="float-right w-1/5 mb-4">
-                <button class="container px-4 py-2 mt-4 bg-theme-yellow hover:bg-yellow-300 text-theme-white rounded-md">変更</button>
+                    </div>
+                    <!--modal footer-->
+                    <div class="flex flex-row justify-end items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600 gap-6">
+                        <button type="submit" class="text-white bg-sky-600 hover:bg-sky-400 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-600 dark:focus:ring-blue-400 w-1/5">登録</button>
+                    </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -43,13 +43,13 @@
 
     <!--content-->
     <div class="flex justify-center items-center pt-20">
-        <div class="flex justify-center items-center w-full h-modal md:h-full">
+        <div class="flex justify-center items-center w-1/3 h-modal md:h-full">
             <div class="relative p-4 w-full max-w-3xl h-full md:h-auto">
                 <!--modal content-->
                 <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                     <!--modal header-->
                     <div class="flex justify-between items-center p-5 rounded-t border-b dark:border-gray-600">
-                        <h3 class="text-xl font-bold text-cyan-600 dark:text-white">
+                        <h3 class="text-xl font-bold text-sky-600 dark:text-white">
                             無料会員登録
                         </h3>
 
@@ -70,7 +70,7 @@
                                 <input type="text" name="address" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                                 <label for="address" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">会社名（任意）</label>
                             </div>
-                            <div class="relative z-0 w-full mb-4 group">
+                            <!--<div class="relative z-0 w-full mb-4 group">
                                 <input type="text" name="phone_number" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                                 <label for="phone_number" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">電話番号</label>
                             </div>
@@ -93,7 +93,7 @@
                                     <input type="password" name="password_confirmation" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                                     <label for="password_confirmation" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">パスワードを認証する</label>
                                 </div>
-                            </div>
+                            </div>-->
                             <div class="relative z-0 w-full mb-4 group">
                                 <x-jet-validation-errors class="mb-4" />
                             </div>
@@ -103,9 +103,9 @@
                     <div class="flex flex-row justify-end items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600 gap-6">
                         <div class="flex flex-row gap-2">
                             <span>すでに登録？</span>
-                            <a href="{{route('login')}}" class="text-blue-600 hover:text-blue-400 underline underline-offset-2">ログインする</a>
+                            <a href="{{route('login')}}" class="text-sky-600 hover:text-sky-400 underline underline-offset-2">ログインする</a>
                         </div>
-                        <button type="submit" class="text-white bg-blue-600 hover:bg-blue-400 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-600 dark:focus:ring-blue-400 w-1/5">登録</button>
+                        <button type="submit" class="text-white bg-sky-600 hover:bg-sky-400 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-600 dark:focus:ring-blue-400 w-1/5">登録</button>
                     </div>
                     </form>
                 </div>
