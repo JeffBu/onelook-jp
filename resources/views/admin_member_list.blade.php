b5a545347fe1729f269dc498a16987f724274df0
jdecena08 <jaezoff.d@jvn-cgs.com>
2022-07-27 10:38:51 +0800

    gonna pull, don't add to changelog

--- resources/views/admin_member_list.blade.php
+++ resources/views/admin_member_list.blade.php
@@ -29,7 +29,6 @@
 </head>
 
 <body class="justify-center shadow-sm items-center bg-theme-white text-theme-black font-['Calibri']">
-
     <!--content-->
     <div class="flex flex-row justify-center items-start w-full">
         <nav id="menu" class="hidden sm:flex flex-col px-2 justify-left items-center text-center w-72 nav-bg shadow-md divide-y divide-theme-white min-h-screen fixed md:sticky top-0 left-0">
@@ -48,7 +47,7 @@
                     <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
                 </svg> ホーム</a>
 
-                <a href="#" class="flex px-4 py-2 justify-left items-center text-base font-semibold text-theme-white gap-3 w-full" id="member-list"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <a href="{{route('admin-member-list')}}" class="flex px-4 py-2 justify-left items-center text-base font-semibold text-theme-white gap-3 w-full" id="member-list"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                     <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
                 </svg> 会員一覧</a>
 
