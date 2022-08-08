78e00983451db30a59b496a641da0a68bc754b27
jdecena08 <jaezoff.d@jvn-cgs.com>
2022-08-08 14:46:11 +0800

    added: admin_posting

--- resources/views/admin/contents/admin_posting.blade.php
+++ resources/views/admin/contents/admin_posting.blade.php
@@ -12,7 +12,7 @@
             <button id="menu-button" onclick="toggleMenu()"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
             </svg></button>
-            <h1 class="text-lg font-semibold text-theme-white">ニュースと投稿</h1>
+            <h1 class="text-lg font-semibold text-theme-white">お知らせと投稿履歴</h1>
         </div>
         <h1 class="text-lg font-semibold text-theme-white">管理画面</h1>
     </div>
