0743e8ba8b7b0a70aba1924b96d19656ce666a1b
jdecena08 <jaezoff.d@jvn-cgs.com>
2022-08-30 14:58:20 +0800

    updated: dashboard page

--- resources/views/authenticated-user/contents/dashboard.blade.php
+++ resources/views/authenticated-user/contents/dashboard.blade.php
@@ -53,7 +53,7 @@
                             <tbody>
                                 @forelse($news as $item)
                                     <tr>
-                                        <td id="news-date" class="text-xs pt-2">{{$item->created_at->format('Y年m月d日')}} @if($item->target_user) <span class="px-2 py-1 rounded-full border border-2 bg-emerald-100 border-emerald-600 text-emerald-600">管理者からのお知らせ</span> @endif</td>
+                                        <td id="news-date" class="text-xs pt-2">{{$item->created_at->format('Y年m月d日')}} @if($item->target_user) <span class="px-2 py-1 rounded-full border-2 bg-emerald-100 border-emerald-600 text-emerald-600">管理者からのお知らせ</span> @endif</td>
                                     </tr>
                                     <tr>
                                         <td id="news-label" class="border-b border-sky-800 pb-2">{!!$item->content!!}</td>
