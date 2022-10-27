7d822623bfe56a831b7ae0e225cae2d64f3a4892
jdecena08 <jaezoff.d@jvn-cgs.com>
2022-10-27 16:11:33 +0800

    bug fixed 10-27-2022

--- resources/views/authenticated-user/contents/subscription_2.blade.php
+++ resources/views/authenticated-user/contents/subscription_2.blade.php
@@ -134,6 +134,23 @@
         })
 
         function changePlan() {
+            // Swal.fire({
+            //     title: 'Are you sure?',
+            //     text: "You won't be able to revert this!",
+            //     icon: 'warning',
+            //     showCancelButton: true,
+            //     confirmButtonColor: '#3085d6',
+            //     cancelButtonColor: '#d33',
+            //     confirmButtonText: 'Yes, delete it!'
+            //     }).then((result) => {
+            //     if (result.isConfirmed) {
+            //         Swal.fire(
+            //         'Deleted!',
+            //         'Your file has been deleted.',
+            //         'success'
+            //         )
+            //     }
+            // })
             $('#upgrade_plan').css('display', 'block')
             $('#select_button_text').html('パーソナルプランの申込')
             $('#cancel_service').css('display', 'none')
