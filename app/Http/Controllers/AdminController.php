2801303f5e9d983c01627e50395eb9ce9033d7bd
jdecena08 <jaezoff.d@jvn-cgs.com>
2022-11-11 15:16:33 +0800

    bug fixed 11-11-2022 v1

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -113,10 +113,11 @@ class AdminController extends Controller
             foreach ($users as $user)
             {   
                 $subs = Subscription::where('user_id',$user->id)->get();
+                $subs2 = Subscription::where('user_id',$user->id)->first();
                 $url = url('/admin-member-info')."?user_id=".$user->id;
                 $nestedData['name'] = '<a href="'.$url.'" class="text-blue-600 hover:text-blue-400 underline underline-offset-2">'.$user->name.'</a>';
                 $nestedData['created_at'] = $user->created_at->format('Y年m月d日');
-                $nestedData['subscription_date'] = ($subs->isEmpty()) ? '---' : $subs->created_at;
+                $nestedData['subscription_date'] = ($subs->isEmpty()) ? '---' : $subs2->created_at->format('Y年m月d日');
                 $nestedData['membership_type'] =  ($subs->isEmpty()) ? '無料プラン' : 'パーソナルプラン';
                 $nestedData['create_video'] = "";
                 $nestedData['in_time_video'] = "";
